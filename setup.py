365ca4ba83af5e86eb2143e7f069862cec7d2477
Tom French <tom@tomfren.ch>
2018-10-14 23:31:10 +0100

    Hotfix: Added missing underscore to PowerLED._led_id

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nuc_led',
-      version='0.1',
+      version='0.1.1',
       description='A python API for control of the front LEDs of Intel NUC7i[x]BN and NUC6CAY NUCs.',
       url='https://github.com/Solije/intel_nuc_led_python_api',
       author='Tom French',
